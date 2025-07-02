ac93c39b23c07188d61df2d047b398e5512f8811
vitorhugonogueira <vhnogueira@outlook.com>
2025-07-01 21:00:41 -0300

    Feat: Including String.fromEnvironment for env props.

--- lib/data/data_config.dart
+++ lib/data/data_config.dart
@@ -1,11 +1,25 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+const defaultURL = 'http://localhost:3000';
+
 class DataConfig {
   static String get apiUrl {
-    return dotenv.env['API_URL'] ?? 'http://localhost:3000';
+    const String enviromentApiUrl = String.fromEnvironment('API_URL');
+    if (enviromentApiUrl.isNotEmpty) {
+      return enviromentApiUrl;
+    }
+
+    return dotenv.env['API_URL'] ?? defaultURL;
   }
 
   static String get productImagesUrl {
-    return dotenv.env['PRODUCT_IMAGES_URL'] ?? 'http://localhost:3000';
+    const String enviromentProductImagesUrl = String.fromEnvironment(
+      'PRODUCT_IMAGES_URL',
+    );
+    if (enviromentProductImagesUrl.isNotEmpty) {
+      return enviromentProductImagesUrl;
+    }
+
+    return dotenv.env['PRODUCT_IMAGES_URL'] ?? defaultURL;
   }
 }
