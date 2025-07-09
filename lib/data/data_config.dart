5b28ef34ce625a43f78d8bdfd9c54be1af9d216d
vitorhugonogueira <vhnogueira@outlook.com>
2025-07-09 11:46:19 -0300

    Fix: Data config String Enviroment.

--- lib/data/data_config.dart
+++ lib/data/data_config.dart
@@ -1,14 +1,13 @@
 const defaultURL = 'http://localhost:3000';
 
 class DataConfig {
-  static String get apiUrl {
-    return String.fromEnvironment('API_URL', defaultValue: defaultURL);
-  }
+  static const String apiUrl = String.fromEnvironment(
+    'API_URL',
+    defaultValue: defaultURL,
+  );
 
-  static String get productImagesUrl {
-    return String.fromEnvironment(
-      'PRODUCT_IMAGES_URL',
-      defaultValue: defaultURL,
-    );
-  }
+  static const String productImagesUrl = String.fromEnvironment(
+    'PRODUCT_IMAGES_URL',
+    defaultValue: defaultURL,
+  );
 }
