e41e96bd77f490e492c9717cd5fb36f15345c028
vitorhugonogueira <vhnogueira@outlook.com>
2025-07-23 12:34:41 -0300

    Test: RemoveItemToCartUseCase.

---
+++ test/domain/usecases/cart/remove_item_to_cart_usecase_test.dart
@@ -0,0 +1,81 @@
+import 'package:clean_ecommerce/domain/models/cart.dart';
+import 'package:clean_ecommerce/domain/models/item.dart';
+import 'package:clean_ecommerce/domain/states/cart_details_state.dart';
+import 'package:clean_ecommerce/domain/usecases/cart/remove_item_to_cart_usecase.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../mocks/datasources/cart_datasource_mock.dart';
+import '../../../mocks/generated_mocks.mocks.dart';
+import '../../../mocks/models/product_mock.dart';
+
+void main() {
+  group('RemoveItemToCartUseCase', () {
+    group('Failure on saving', () {
+      final dialog = MockDialogMockito();
+      final presenter = MockCartDetailsPresenterMockito();
+      final usecase = RemoveItemToCartUseCase(
+        cartRepository: CartDataSourceMock.saveFail(
+          'There was a bug on saving this removing.',
+          null,
+        ),
+        dialog: dialog,
+        presenter: presenter,
+      );
+      usecase.execute(
+        productId: '1',
+        cart: Cart(
+          items: [
+            Item(product: product1, quantity: 2),
+            Item(product: product2, quantity: 4),
+          ],
+        ),
+      );
+      test('Should inform error message', () {
+        verify(
+          dialog.showError('There was a bug on saving this removing.'),
+        ).called(1);
+      });
+      test('Should NOT present cart details state', () {
+        verifyNever(presenter.show(any));
+      });
+    });
+    group('Success on saving', () {
+      final dialog = MockDialogMockito();
+      final presenter = MockCartDetailsPresenterMockito();
+      final usecase = RemoveItemToCartUseCase(
+        cartRepository: CartDataSourceMock.saveSuccess(null),
+        dialog: dialog,
+        presenter: presenter,
+      );
+      usecase.execute(
+        productId: '1',
+        cart: Cart(
+          items: [
+            Item(product: product1, quantity: 2),
+            Item(product: product2, quantity: 4),
+          ],
+        ),
+      );
+      test('Should NOT inform error message', () {
+        verifyNever(dialog.showError(any));
+      });
+      test('Should present updated cart rightly', () {
+        verify(
+          presenter.show(
+            argThat(
+              isA<CartDetailsState>()
+                  .having((obj) => obj.cart!.items.length, 'itemsLenght', 1)
+                  .having(
+                    (obj) => obj.cart!.items[0].product.id,
+                    'product',
+                    '2',
+                  )
+                  .having((obj) => obj.cart!.items[0].quantity, 'quantity', 4),
+            ),
+          ),
+        );
+      });
+    });
+  });
+}
